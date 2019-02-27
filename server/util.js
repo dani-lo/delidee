31bdcd5cc41615f6ba61e464ea9d659e48b1e2a8
dani <dani@brightanalytics.co.uk>
2019-02-27 09:33:32 +0000

    push notification settings

--- server/util.js
+++ server/util.js
@@ -18,7 +18,7 @@ const alertUsers = (newOrder) => {
     message : "New Order",
     title   : "New Order From User " + orderUser,
     sound   : 'persistent',
-    device  : 'daniphone',
+    device  : 'daniphone,Aim178915,siam01',
     priority: 2,
     retry   : 30,
     expire  : 120
